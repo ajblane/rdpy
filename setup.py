3a33938edf76f8ed5fc8832da57f7aab52fa317a
Eyal Karni <ekarni@preempt.com>
2018-04-03 19:07:23 +0300

    Initial readme and setup works and fix indent

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(name='rdpy',
 	description='Remote Desktop Protocol in Python',
 	long_description="""
 	RDPY is a pure Python implementation of the Microsoft RDP (Remote Desktop Protocol) protocol (Client and Server side). RDPY is built over the event driven network engine Twisted.
-	
 	RDPY provide RDP and VNC binaries : RDP Man In The Middle proxy which record session, RDP Honeypot, RDP screenshoter, RDP client, VNC client, VNC screenshoter, RSS Player
 	""",
 	author='Sylvain Peyrefitte',
@@ -23,18 +22,9 @@ setup(name='rdpy',
 			'rdpy.protocol.rdp.pdu', 
 			'rdpy.protocol.rdp.nla',
 			'rdpy.protocol.rdp.t125',
-			'rdpy.protocol.rfb', 
-			'rdpy.ui'
 		],
-	ext_modules=[Extension('rle', ['ext/rle.c'])],
 	scripts = [
-			'bin/rdpy-rdpclient.py',
-			'bin/rdpy-rdphoneypot.py',
-			'bin/rdpy-rdpmitm.py',
-			'bin/rdpy-rdpscreenshot.py', 
-			'bin/rdpy-rssplayer.py',
-			'bin/rdpy-vncclient.py', 
-			'bin/rdpy-vncscreenshot.py'
+			'./bin/rdpy-rpdcredsspmitm.py',
 		],
 	install_requires=[
 			'twisted',
@@ -43,5 +33,6 @@ setup(name='rdpy',
           	'qt4reactor',
           	'rsa',
           	'pyasn1',
+            'impacket'
 	  	],
 )
